12907172a6371fa7042f5035d972af9a3ff3ab1d
nnfuad <nnfuad01@gmail.com>
2026-05-09 02:02:49 +0600

    changed nodemailer with resend

--- server/src/config/mailer.js
+++ server/src/config/mailer.js
@@ -1,13 +1,19 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // important: false for port 587
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false, // important: false for port 587
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS
+//   }
+// });
 
-module.exports = transporter;
+// module.exports = transporter;
+
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+module.exports = resend;
