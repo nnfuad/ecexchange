84140f80192204143b89c9e4beec3f4718c150d1
nnfuad <nnfuad01@gmail.com>
2026-05-30 13:02:34 +0600

    force ipv4 smtp

--- server/src/config/mailer.js
+++ server/src/config/mailer.js
@@ -1,18 +1,31 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
-  connectionTimeout: 10000
+  tls: {
+    rejectUnauthorized: false
+  },
+  family: 4
+});
+
+transporter.verify((err) => {
+  if (err) {
+    console.log("SMTP ERROR:", err);
+  } else {
+    console.log("SMTP READY");
+  }
 });
 
 module.exports = transporter;
-
 // const { Resend } = require("resend");
 
 // const resend = new Resend(process.env.RESEND_API_KEY);
