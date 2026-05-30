d064661be49227e09607fcbee0a1519cc91a2b97
nnfuad <nnfuad01@gmail.com>
2026-05-30 12:50:39 +0600

    nodemailer updated

--- server/src/config/mailer.js
+++ server/src/config/mailer.js
@@ -1,13 +1,11 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // important: false for port 587
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 module.exports = transporter;
